fd6557e7e68878d4b8d207edf22af39867b07cad
Matthew Brett <matthew.brett@gmail.com>
2016-07-20 15:47:31 +0100

    Give up on building numpy wheel for OSX

--- config.sh
+++ config.sh
@@ -4,14 +4,14 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    if [ -n "$IS_OSX" ]; then
-        # Compile our own numpy from source (using Accelerate)
-        pip install $NP_BUILD_DEP --no-use-wheel
-    fi
+    :
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     nosetests --exe -v sklearn
+    if [ -n "$IS_OSX" ]; then  # Run 32-bit tests on dual arch wheel
+        arch -i386 nosetests --exe -v sklearn
+    fi
 }
