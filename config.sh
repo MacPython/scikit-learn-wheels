1a0243f0e6091f013fb1de1d5a241795c7491a92
Matthew Brett <matthew.brett@gmail.com>
2016-07-20 13:56:59 +0100

    Try building with multibuild

---
+++ config.sh
@@ -0,0 +1,17 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    if [ -n "$IS_OSX" ]; then
+        # Compile our own numpy from source (using Accelerate)
+        pip install numpy==$NUMPY_VERSION --no-use-wheel
+    fi
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    nosetests --exe -v sklearn
+}
