5e3fb93982f419ae7f020efd363fd6fd4e78e446
Matthew Brett <matthew.brett@gmail.com>
2016-07-20 14:35:12 +0100

    Fix numpy build dependency variable for OSX

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ function pre_build {
     # Runs in the root directory of this repository.
     if [ -n "$IS_OSX" ]; then
         # Compile our own numpy from source (using Accelerate)
-        pip install numpy==$NUMPY_VERSION --no-use-wheel
+        pip install numpy==$NP_BUILD_DEP --no-use-wheel
     fi
 }
 
