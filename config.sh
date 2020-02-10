45b9d29d0dd7e731e11063700fd4ae0c9ced4004
Olivier Grisel <olivier.grisel@ensta.org>
2020-02-10 10:51:12 +0100

    Use additional shell functions to be used directly in main posix.yml scripts

--- config.sh
+++ config.sh
@@ -12,37 +12,3 @@ function run_tests {
     python --version
     pytest -l --pyargs sklearn
 }
-
- function enable_openmp {
-    # Install OpenMP
-    brew install libomp
-    export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
-    export CFLAGS="$CFLAGS -I/usr/local/opt/libomp/include"
-    export CXXFLAGS="$CXXFLAGS -I/usr/local/opt/libomp/include"
-    export LDFLAGS="$LDFLAGS -L/usr/local/opt/libomp/lib -lomp"
-    export DYLD_LIBRARY_PATH=/usr/local/opt/libomp/lib
-}
-
-function disable_system_openmp {
-    brew uninstall libomp
-}
-
-function setup_test_venv {
-    # Create a new empty venv dedicated to testing for non-Linux platforms. On
-    # Linux the tests are run in a Docker container.
-    if [ $(uname) != "Linux" ]; then
-        source deactivate
-        virtualenv --python=python test_venv
-        source test_venv/bin/activate
-        python --version # just to check
-        pip install --upgrade pip wheel
-        pip install $TEST_DEPENDS
-    fi
-}
-
-function teardown_test_venv {
-    if [ $(uname) != "Linux" ]; then
-        source deactivate
-        source venv/bin/activate
-    fi
-}
---
+++ extra_functions.sh
@@ -0,0 +1,38 @@
+
+ function setup_compiler {
+    # Install OpenMP support on macOS
+    if [ $(uname) == "Darwin" ]; then
+        brew install libomp
+        export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
+        export CFLAGS="$CFLAGS -I/usr/local/opt/libomp/include"
+        export CXXFLAGS="$CXXFLAGS -I/usr/local/opt/libomp/include"
+        export LDFLAGS="$LDFLAGS -L/usr/local/opt/libomp/lib -lomp"
+        export DYLD_LIBRARY_PATH=/usr/local/opt/libomp/lib
+    fi
+}
+
+function disable_system_openmp {
+    if [ $(uname) == "Darwin" ]; then
+        brew uninstall libomp
+    fi
+}
+
+function setup_test_venv {
+    # Create a new empty venv dedicated to testing for non-Linux platforms. On
+    # Linux the tests are run in a Docker container.
+    if [ $(uname) != "Linux" ]; then
+        deactivate
+        virtualenv --python=python test_venv
+        source test_venv/bin/activate
+        python --version # just to check
+        pip install --upgrade pip wheel
+        pip install $TEST_DEPENDS
+    fi
+}
+
+function teardown_test_venv {
+    if [ $(uname) != "Linux" ]; then
+        deactivate
+        source venv/bin/activate
+    fi
+}
