e828aedc39823d02a55b33903379c55fa79b6022
Olivier Grisel <olivier.grisel@ensta.org>
2020-02-10 10:09:03 +0100

    Isolate tests in empty venv and let pip install latest runtime deps

--- config.sh
+++ config.sh
@@ -26,3 +26,23 @@ function run_tests {
 function disable_system_openmp {
     brew uninstall libomp
 }
+
+function setup_test_venv {
+    # Create a new empty venv dedicated to testing for non-Linux platforms. On
+    # Linux the tests are run in a Docker container.
+    if [ $(uname) != "Linux" ]; then
+        source deactivate
+        virtualenv --python=python test_venv
+        source test_venv/bin/activate
+        python --version # just to check
+        pip install --upgrade pip wheel
+        pip install $TEST_DEPENDS
+    fi
+}
+
+function teardown_test_venv {
+    if [ $(uname) != "Linux" ]; then
+        source deactivate
+        source venv/bin/activate
+    fi
+}
