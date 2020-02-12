b10cb88e586fd2bfb25bff6f72ab5cbb239f76db
Olivier Grisel <olivier.grisel@ensta.org>
2020-02-12 09:51:16 +0100

    Debug

--- config.sh
+++ config.sh
@@ -10,5 +10,5 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    pytest -l --junitxml=$JUNITXML --pyargs sklearn
+    pytest -l -k isotonic --junitxml=$JUNITXML --pyargs sklearn
 }
