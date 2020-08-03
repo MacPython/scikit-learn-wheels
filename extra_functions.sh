be87748e8aa82ccac6490f31bece007eae3b2360
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-08-03 14:52:27 +0200

    MNT always update pip

--- extra_functions.sh
+++ extra_functions.sh
@@ -31,13 +31,13 @@ function setup_test_venv {
         # Defined python to be within this env
         PYTHON_EXE=`which python`
         PIP_CMD="$PYTHON_EXE -mpip"
-        # Note: the idiom "python -m pip install ..." is necessary to upgrade
-        # pip itself on Windows. Otherwise one would get a permission error on
-        # pip.exe.
-        python -m pip install --upgrade pip wheel
-        if [ "$TEST_DEPENDS" != "" ]; then
-            pip install $TEST_DEPENDS
-        fi
+    fi
+    # Note: the idiom "python -m pip install ..." is necessary to upgrade
+    # pip itself on Windows. Otherwise one would get a permission error on
+    # pip.exe.
+    python -m pip install --upgrade pip wheel
+    if [ "$TEST_DEPENDS" != "" ]; then
+        pip install $TEST_DEPENDS
     fi
 }
 
