755a653836482b50440fd3890eb08cb8d8594517
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-08-03 16:23:27 +0200

    debug

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    python -m pip install --upgrade pip
+    :
 }
 
 # XXX: test-data.xml is hard-coded because the JUNITXML env variable
@@ -13,4 +13,30 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     pytest -l --junitxml=test-data.xml --pyargs sklearn
+}
+
+
+function install_wheel {
+    # Install test dependencies and built wheel
+    #
+    # Pass any input flags to pip install steps
+    #
+    # Depends on:
+    #     WHEEL_SDIR  (optional, default "wheelhouse")
+    #     TEST_DEPENDS  (optional, default "")
+    #     MANYLINUX_URL (optional, default "") (via pip_opts function)
+    #
+    # XXX:
+    # Override install_wheel from common_utils.sh to force upgrade pip
+    # prior to installing test dependencies in the docker environment.
+    python -m pip install --upgrade pip
+    local wheelhouse=$(abspath ${WHEEL_SDIR:-wheelhouse})
+    if [ -n "$TEST_DEPENDS" ]; then
+        while read TEST_DEPENDENCY; do
+            pip install $(pip_opts) $@ $TEST_DEPENDENCY
+        done <<< "$TEST_DEPENDS"
+    fi
+    # Install compatible wheel
+    pip install $(pip_opts) $@ \
+        $(python $MULTIBUILD_DIR/supported_wheels.py $wheelhouse/*.whl)
 }
